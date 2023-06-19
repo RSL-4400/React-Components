c305a83f6b2b81dad7d564ba1df3dfaf471ad85d
RSL-4400 <135926079+RSL-4400@users.noreply.github.com>
2023-06-18 23:13:57 -0400

    React Components Commit

---
+++ src/Components/myfirstcomponent/Tag.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Tag.css';
+
+const Tag = () => {
+    return (
+        <div>
+            <h2>Do you want to learn React</h2>
+        </div>
+    );
+};
+
+export default Tag;
---
+++ src/Components/mysecondcomponent/Link.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Link.css';
+const Link = () => {
+    return (
+    <div>
+        <a href="https://react.dev/"><button class="button"><b>Allow Me</b></button></a>  
+    </div>    
+    );
+};
+ export default Link;
